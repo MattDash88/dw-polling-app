1299a9a05592253431b341396d2703b25d801918
MattDash88 <MattDash@protonmail.com>
2021-06-27 23:40:18 +0200

    Added logic for name3 and some other fixes

--- components/client_components/CandidateList.js
+++ components/client_components/CandidateList.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { 
+import {
   Grid,
   Checkbox,
+  Icon,
+  Popup,
 } from 'semantic-ui-react';
 
 const CandidateList = props => {
@@ -10,6 +12,8 @@ const CandidateList = props => {
     let label = text;
     if (alias.length > 0) {
       label = `${text} - ${alias}`;
+    } else if (key == 'name3') {
+      label = `${text} - (The candidate requested to keep their full name confidential unless they are elected)`;
     }
     return (
       <Grid.Row key={key}>
--- components/headers/IndexHeader.js
+++ components/headers/IndexHeader.js
@@ -11,7 +11,7 @@ export default class PageHeader extends React.Component {
                     <link rel="manifest" type="image/x-icon" href="/static/manifest.json" />    
                     <link rel="shortcut icon" type="image/x-icon" href="/static/icons/favicon.ico" />                    
                     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-                    <title>2021 Dash Trust Protector Elections</title>
+                    <title>2021 DIF Supervisor Elections</title>
                 </Head>
             </div>
         )
